a654e055ee8978a8370f766559d1fd93d003f3a2
Ricard-Ruiz-TCM <83354017+Ricard-Ruiz-TCM@users.noreply.github.com>
2023-02-08 15:24:15 +0100

    Proyecto + algunos puntos ;3

---
+++ gameManager/gameManager.go
@@ -0,0 +1,30 @@
+components {
+  id: "gameManager"
+  component: "/gameManager/gameManager.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "canvas"
+  component: "/main/canvas.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
