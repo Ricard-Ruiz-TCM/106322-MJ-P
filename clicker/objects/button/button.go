7fac2d1da53757f7f978fe336c8e70d7a1e6651f
Ricard-Ruiz-TCM <83354017+Ricard-Ruiz-TCM@users.noreply.github.com>
2023-02-09 20:41:42 +0100

    completo, comentarios y cositas mejoradas

---
+++ clicker/objects/button/button.go
@@ -0,0 +1,58 @@
+components {
+  id: "button"
+  component: "/objects/button/button.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/resources/atlas/atlas.atlas\"\n"
+  "default_animation: \"button\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sfxClick"
+  type: "sound"
+  data: "sound: \"/assets/resources/sound/sfx/click.wav\"\n"
+  "looping: 0\n"
+  "group: \"button\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
