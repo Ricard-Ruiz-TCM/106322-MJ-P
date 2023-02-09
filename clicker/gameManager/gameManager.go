01fdca2850cbe2ff0c95e3e417ee6cdf8a579310
Ricard-Ruiz-TCM <83354017+Ricard-Ruiz-TCM@users.noreply.github.com>
2023-02-09 19:14:59 +0100

    re-struct

---
+++ clicker/gameManager/gameManager.go
@@ -0,0 +1,45 @@
+components {
+  id: "gameManager"
+  component: "/gameManager/gameManager.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "ten"
+  component: "/assets/prefabs/ten.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "backgroundMusic"
+  component: "/gameManager/backgroundMusic.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
